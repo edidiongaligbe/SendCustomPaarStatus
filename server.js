4d68a8ce44a01730b4b1864c848e14fbd9c36647
edidiongaligbe <edidiong.aligbe@gmail.com>
2022-02-22 10:29:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/api/sendpaar', function (req, res) {
       from: "testingapps@zohomail.com", // sender address
       to: `${req.body.email}`,
       subject: "PAAR Query Status", // Subject line
-      html: `<p>Hi,</p><p>The status of your PAAR query is <b>${req.body.paarstatus}</b></p>`, // plain text body
+      html: `<p>Hi,</p><p>The status of your PAAR query is <b>${req.body.paar}</b></p>`, // plain text body
     };
   
     transporter.sendMail(mailOptions).then(()=>{
